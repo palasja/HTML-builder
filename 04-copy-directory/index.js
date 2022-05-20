8d06a208a7bdae169b4f9fc5206dff23bc44377c
Yakubenka Ivan <palasja@gmail.com>
2022-05-20 17:30:34 +0300

    fix: add delete from copy in 3

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,26 +2,48 @@ const path = require('path');
 const fs = require('fs');
 const readdir = require('fs/promises');
 const {stdout} = process;
-await fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, err => {});
+
 fs.mkdir(path.join(__dirname, 'files-copy'), err => {});
 copyDir('files','files-copy');
+clearCopyFolder('files','files-copy');
 
 function copyDir(folder, copyFolder){
     fs.readdir(path.join(__dirname, folder), {withFileTypes: true}, (err, dir) => {
         if (err) {
             throw err;
         }
+        const fullPath = path.join(__dirname, folder, d.name);
+        const copyPath = path.join(__dirname, copyFolder, d.name);
         dir.forEach( d => {
             if(d.isFile()){
-                const fullPath = path.join(__dirname, folder, d.name);
-                const copyPath = path.join(__dirname, copyFolder, d.name);
                 fs.copyFile(fullPath, copyPath, err => {})
             }
             if(d.isDirectory()){
-                const fullPath = path.join(__dirname, folder, d.name);
-                const copyPath = path.join(__dirname, copyFolder, d.name);
                 fs.mkdir(copyPath, err => {});
                 copyDir(fullPath, copyPath); 
+            }
+        });
+    });
+}
+
+function clearCopyFolder(folder, copyFolder){
+    fs.readdir(path.join(__dirname, copyFolder), {withFileTypes: true}, (err, dir) => {
+        if (err) {
+            throw err;
+        }
+        const fullPath = path.join(__dirname, folder, d.name);
+        const copyPath = path.join(__dirname, copyFolder, d.name);
+        dir.forEach( d => {
+            if(d.isFile()){
+                
+                fs.access(fullPath, fs.constants.F_OK, (err) => {
+                    if(err) fs.rm(copyPath, err => {});
+                  });
+            }
+            if(d.isDirectory()){
+                fs.access(fullPath, fs.constants.F_OK, (err) => {
+                    if(err) fs.rm(copyPath, { recursive: true },err => {});
+                  });
             }
         });
     });
